a064dd8ee66eb58f27b3f4c0d435d6612d15dc5f
LAPTOP-PC70JGRA\Asus <onkar.gavit@thoughtsketch.com>
2026-04-05 11:53:04 +0530

    resoloved-seller-login-flow

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import GlobalStyles from './index.tsx'
-import { AuthProvider } from './contexts/AuthContext'
-import { AppRoutes } from './router'
-import './utils/apiInterceptors'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import GlobalStyles from "./index.tsx";
+import { AuthProvider } from "./contexts/AuthContext";
+import { AppRoutes } from "./router";
+import "./utils/apiInterceptors";
 
 const RootLayout = () => (
   <AuthProvider>
     <GlobalStyles />
     <AppRoutes />
   </AuthProvider>
-)
+);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <RootLayout />
     </HashRouter>
   </StrictMode>,
-)
+);
